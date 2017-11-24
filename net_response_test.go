574c3a93c53b0a158bbbf0643ed09a8753f51d39
Randy Coburn <morfien10@gmail.com>
2017-11-24 14:37:02 +0000

    Fixing tests using the new test methods available in infludLineProtocolOutput

--- net_response_test.go
+++ net_response_test.go
@@ -6,275 +6,13 @@ import (
 	"testing"
 	"time"
 
-	"github.com/influxdata/telegraf/internal"
-	"github.com/influxdata/telegraf/testutil"
+	"github.com/morfien101/influxLineProtocolOutput"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSample(t *testing.T) {
-	c := &NetResponse{}
-	output := c.SampleConfig()
-	if output != sampleConfig {
-		t.Error("Sample config doesn't match")
-	}
-}
-
-func TestDescription(t *testing.T) {
-	c := &NetResponse{}
-	output := c.Description()
-	if output != description {
-		t.Error("Description output is not correct")
-	}
-}
-func TestBadProtocol(t *testing.T) {
-	var acc testutil.Accumulator
-	// Init plugin
-	c := NetResponse{
-		Protocol: "unknownprotocol",
-		Address:  ":9999",
-	}
-	// Error
-	err1 := c.Gather(&acc)
-	require.Error(t, err1)
-	assert.Equal(t, "Bad protocol", err1.Error())
-}
-
-func TestNoPort(t *testing.T) {
-	var acc testutil.Accumulator
-	c := NetResponse{
-		Protocol: "tcp",
-		Address:  ":",
-	}
-	err1 := c.Gather(&acc)
-	require.Error(t, err1)
-	assert.Equal(t, "Bad port", err1.Error())
-}
-
-func TestAddressOnly(t *testing.T) {
-	var acc testutil.Accumulator
-	c := NetResponse{
-		Protocol: "tcp",
-		Address:  "127.0.0.1",
-	}
-	err1 := c.Gather(&acc)
-	require.Error(t, err1)
-	assert.Equal(t, "address 127.0.0.1: missing port in address", err1.Error())
-}
-
-func TestSendExpectStrings(t *testing.T) {
-	var acc testutil.Accumulator
-	tc := NetResponse{
-		Protocol: "udp",
-		Address:  "127.0.0.1:7",
-		Send:     "",
-		Expect:   "toast",
-	}
-	uc := NetResponse{
-		Protocol: "udp",
-		Address:  "127.0.0.1:7",
-		Send:     "toast",
-		Expect:   "",
-	}
-	err1 := tc.Gather(&acc)
-	require.Error(t, err1)
-	assert.Equal(t, "Send string cannot be empty", err1.Error())
-	err2 := uc.Gather(&acc)
-	require.Error(t, err2)
-	assert.Equal(t, "Expected string cannot be empty", err2.Error())
-}
-
-func TestTCPError(t *testing.T) {
-	var acc testutil.Accumulator
-	// Init plugin
-	c := NetResponse{
-		Protocol: "tcp",
-		Address:  ":9999",
-	}
-	// Error
-	err1 := c.Gather(&acc)
-	require.NoError(t, err1)
-	acc.AssertContainsTaggedFields(t,
-		"net_response",
-		map[string]interface{}{
-			"result_code": 1,
-			"result_type": "connection_failed",
-		},
-		map[string]string{
-			"server":      "",
-			"port":        "9999",
-			"protocol":    "tcp",
-			"result_text": "connection_failed",
-		},
-	)
-}
-
-func TestTCPOK1(t *testing.T) {
-	var wg sync.WaitGroup
-	var acc testutil.Accumulator
-	// Init plugin
-	c := NetResponse{
-		Address:     "127.0.0.1:2004",
-		Send:        "test",
-		Expect:      "test",
-		ReadTimeout: internal.Duration{Duration: time.Second * 3},
-		Timeout:     internal.Duration{Duration: time.Second},
-		Protocol:    "tcp",
-	}
-	// Start TCP server
-	wg.Add(1)
-	go TCPServer(t, &wg)
-	wg.Wait()
-	// Connect
-	wg.Add(1)
-	err1 := c.Gather(&acc)
-	wg.Wait()
-	// Override response time
-	for _, p := range acc.Metrics {
-		p.Fields["response_time"] = 1.0
-	}
-	require.NoError(t, err1)
-	acc.AssertContainsTaggedFields(t,
-		"net_response",
-		map[string]interface{}{
-			"result_code":   0,
-			"result_type":   "success",
-			"string_found":  true,
-			"response_time": 1.0,
-		},
-		map[string]string{
-			"result_text": "success",
-			"server":      "127.0.0.1",
-			"port":        "2004",
-			"protocol":    "tcp",
-		},
-	)
-	// Waiting TCPserver
-	wg.Wait()
-}
-
-func TestTCPOK2(t *testing.T) {
-	var wg sync.WaitGroup
-	var acc testutil.Accumulator
-	// Init plugin
-	c := NetResponse{
-		Address:     "127.0.0.1:2004",
-		Send:        "test",
-		Expect:      "test2",
-		ReadTimeout: internal.Duration{Duration: time.Second * 3},
-		Timeout:     internal.Duration{Duration: time.Second},
-		Protocol:    "tcp",
-	}
-	// Start TCP server
-	wg.Add(1)
-	go TCPServer(t, &wg)
-	wg.Wait()
-	// Connect
-	wg.Add(1)
-	err1 := c.Gather(&acc)
-	wg.Wait()
-	// Override response time
-	for _, p := range acc.Metrics {
-		p.Fields["response_time"] = 1.0
-	}
-	require.NoError(t, err1)
-	acc.AssertContainsTaggedFields(t,
-		"net_response",
-		map[string]interface{}{
-			"result_code":   1,
-			"result_type":   "string_mismatch",
-			"string_found":  false,
-			"response_time": 1.0,
-		},
-		map[string]string{
-			"result_text": "string_mismatch",
-			"server":      "127.0.0.1",
-			"port":        "2004",
-			"protocol":    "tcp",
-		},
-	)
-	// Waiting TCPserver
-	wg.Wait()
-}
-
-func TestUDPError(t *testing.T) {
-	var acc testutil.Accumulator
-	// Init plugin
-	c := NetResponse{
-		Address:  ":9999",
-		Send:     "test",
-		Expect:   "test",
-		Protocol: "udp",
-	}
-	// Gather
-	err1 := c.Gather(&acc)
-	// Override response time
-	for _, p := range acc.Metrics {
-		p.Fields["response_time"] = 1.0
-	}
-	// Error
-	require.NoError(t, err1)
-	acc.AssertContainsTaggedFields(t,
-		"net_response",
-		map[string]interface{}{
-			"result_code":   1,
-			"result_type":   "read_failed",
-			"response_time": 1.0,
-		},
-		map[string]string{
-			"result_text": "read_failed",
-			"server":      "",
-			"port":        "9999",
-			"protocol":    "udp",
-		},
-	)
-}
-
-func TestUDPOK1(t *testing.T) {
-	var wg sync.WaitGroup
-	var acc testutil.Accumulator
-	// Init plugin
-	c := NetResponse{
-		Address:     "127.0.0.1:2004",
-		Send:        "test",
-		Expect:      "test",
-		ReadTimeout: internal.Duration{Duration: time.Second * 3},
-		Timeout:     internal.Duration{Duration: time.Second},
-		Protocol:    "udp",
-	}
-	// Start UDP server
-	wg.Add(1)
-	go UDPServer(t, &wg)
-	wg.Wait()
-	// Connect
-	wg.Add(1)
-	err1 := c.Gather(&acc)
-	wg.Wait()
-	// Override response time
-	for _, p := range acc.Metrics {
-		p.Fields["response_time"] = 1.0
-	}
-	require.NoError(t, err1)
-	acc.AssertContainsTaggedFields(t,
-		"net_response",
-		map[string]interface{}{
-			"result_code":   0,
-			"result_type":   "success",
-			"string_found":  true,
-			"response_time": 1.0,
-		},
-		map[string]string{
-			"result_text": "success",
-			"server":      "127.0.0.1",
-			"port":        "2004",
-			"protocol":    "udp",
-		},
-	)
-	// Waiting TCPserver
-	wg.Wait()
-}
-
+// Helpers
 func UDPServer(t *testing.T, wg *sync.WaitGroup) {
 	udpAddr, _ := net.ResolveUDPAddr("udp", "127.0.0.1:2004")
 	conn, _ := net.ListenUDP("udp", udpAddr)
@@ -297,4 +35,251 @@ func TCPServer(t *testing.T, wg *sync.WaitGroup) {
 	conn.CloseWrite()
 	tcpServer.Close()
 	wg.Done()
+}
+
+func TestBadProtocol(t *testing.T) {
+	// Init plugin
+	c := NetResponse{
+		Protocol: "unknownprotocol",
+		Address:  ":9999",
+	}
+	// Error
+	err1 := c.gather(influxLineProtocolOutput.New(c.Name))
+	require.Error(t, err1)
+	assert.Equal(t, "Bad protocol", err1.Error())
+}
+
+func TestNoPort(t *testing.T) {
+	c := NetResponse{
+		Protocol: "tcp",
+		Address:  ":",
+	}
+	err1 := c.gather(influxLineProtocolOutput.New(c.Name))
+	require.Error(t, err1)
+	assert.Equal(t, "Bad port", err1.Error())
+}
+
+func TestAddressOnly(t *testing.T) {
+	c := NetResponse{
+		Protocol: "tcp",
+		Address:  "127.0.0.1",
+	}
+	err1 := c.gather(influxLineProtocolOutput.New(c.Name))
+	require.Error(t, err1)
+	assert.Equal(t, "address 127.0.0.1: missing port in address", err1.Error())
+}
+
+func TestSendExpectStrings(t *testing.T) {
+	tc := NetResponse{
+		Name:     "tcp",
+		Protocol: "udp",
+		Address:  "127.0.0.1:7",
+		Send:     "",
+		Expect:   "toast",
+	}
+	uc := NetResponse{
+		Name:     "udp",
+		Protocol: "udp",
+		Address:  "127.0.0.1:7",
+		Send:     "toast",
+		Expect:   "",
+	}
+	err1 := tc.gather(influxLineProtocolOutput.New(tc.Name))
+	require.Error(t, err1)
+	assert.Equal(t, "Send string cannot be empty", err1.Error())
+	err2 := uc.gather(influxLineProtocolOutput.New(uc.Name))
+	require.Error(t, err2)
+	assert.Equal(t, "Expected string cannot be empty", err2.Error())
+}
+
+func TestTCPError(t *testing.T) {
+	// Init plugin
+	c := NetResponse{
+		Name:     "net_response",
+		Protocol: "tcp",
+		Address:  ":9999",
+	}
+	// Error
+	container := influxLineProtocolOutput.New(c.Name)
+	err1 := c.gather(container)
+	require.NoError(t, err1)
+
+	expectedTags := map[string]string{
+		"server":      "",
+		"port":        "9999",
+		"protocol":    "tcp",
+		"result_text": "connection_failed",
+	}
+	expectedValues := map[string]interface{}{
+		"result_code": 1,
+		"result_type": "connection_failed",
+	}
+
+	if err := container.Contains(expectedTags, expectedValues); err != nil {
+		t.Errorf("TestTCPError failed. Error: %s", err)
+	}
+}
+
+func TestTCPOK1(t *testing.T) {
+	var wg sync.WaitGroup
+	// Init plugin
+	c := NetResponse{
+		Name:        "net_responce",
+		Address:     "127.0.0.1:2004",
+		Send:        "test",
+		Expect:      "test",
+		ReadTimeout: Duration{Duration: time.Second * 3},
+		Timeout:     Duration{Duration: time.Second},
+		Protocol:    "tcp",
+	}
+	// Start TCP server
+	wg.Add(1)
+	go TCPServer(t, &wg)
+	wg.Wait()
+	// Connect
+	wg.Add(1)
+	container := influxLineProtocolOutput.New("net_response")
+	err1 := c.gather(container)
+	wg.Wait()
+	// Override response time, due to local testing we get unpredictable response times.
+	container.Values["response_time"] = 1.0
+
+	require.NoError(t, err1)
+
+	expectedValues := map[string]interface{}{
+		"result_code":   0,
+		"result_type":   "success",
+		"string_found":  true,
+		"response_time": 1.0,
+	}
+	expectedTags := map[string]string{
+		"result_text": "success",
+		"server":      "127.0.0.1",
+		"port":        "2004",
+		"protocol":    "tcp",
+	}
+	if err := container.Contains(expectedTags, expectedValues); err != nil {
+		t.Errorf("TCP success doesn't have to the correct values. Error: %s", err)
+	}
+	// Waiting TCPserver
+	wg.Wait()
+}
+
+func TestTCPOK2(t *testing.T) {
+	var wg sync.WaitGroup
+	// Init plugin
+	c := NetResponse{
+		Address:     "127.0.0.1:2004",
+		Send:        "test",
+		Expect:      "test2",
+		ReadTimeout: Duration{Duration: time.Second * 3},
+		Timeout:     Duration{Duration: time.Second},
+		Protocol:    "tcp",
+	}
+	// Start TCP server
+	wg.Add(1)
+	go TCPServer(t, &wg)
+	wg.Wait()
+	// Connect
+	wg.Add(1)
+	container := influxLineProtocolOutput.New("net_response")
+	err1 := c.gather(container)
+	wg.Wait()
+	// Override response time, due to local testing we get unpredictable response times.
+	container.Values["response_time"] = 1.0
+
+	require.NoError(t, err1)
+	expectedValues := map[string]interface{}{
+		"result_code":   1,
+		"result_type":   "string_mismatch",
+		"string_found":  false,
+		"response_time": 1.0,
+	}
+	expectedTags := map[string]string{
+		"result_text": "string_mismatch",
+		"server":      "127.0.0.1",
+		"port":        "2004",
+		"protocol":    "tcp",
+	}
+	if err := container.Contains(expectedTags, expectedValues); err != nil {
+		t.Errorf("TCP success doesn't have to the correct values. Error: %s", err)
+	}
+	// Waiting TCPserver
+	wg.Wait()
+}
+
+func TestUDPError(t *testing.T) {
+	// Init plugin
+	c := NetResponse{
+		Address:  ":9999",
+		Send:     "test",
+		Expect:   "test",
+		Protocol: "udp",
+	}
+	// Gather
+	container := influxLineProtocolOutput.New("net_response")
+	err1 := c.gather(container)
+	// Override response time, due to local testing we get unpredictable response times.
+	container.Values["response_time"] = 1.0
+
+	// Error
+	require.NoError(t, err1)
+	expectedValues := map[string]interface{}{
+		"result_code":   1,
+		"result_type":   "read_failed",
+		"response_time": 1.0,
+	}
+	expectedTags := map[string]string{
+		"result_text": "read_failed",
+		"server":      "",
+		"port":        "9999",
+		"protocol":    "udp",
+	}
+	if err := container.Contains(expectedTags, expectedValues); err != nil {
+		t.Errorf("UDP Error failed to set the correct values. Error: %s", err)
+	}
+}
+
+func TestUDPOK1(t *testing.T) {
+	var wg sync.WaitGroup
+	// Init plugin
+	c := NetResponse{
+		Address:     "127.0.0.1:2004",
+		Send:        "test",
+		Expect:      "test",
+		ReadTimeout: Duration{Duration: time.Second * 3},
+		Timeout:     Duration{Duration: time.Second},
+		Protocol:    "udp",
+	}
+	// Start UDP server
+	wg.Add(1)
+	go UDPServer(t, &wg)
+	wg.Wait()
+	// Connect
+	wg.Add(1)
+	container := influxLineProtocolOutput.New("net_response")
+	err1 := c.gather(container)
+	wg.Wait()
+	// Override response time, due to local testing we get unpredictable response times.
+	container.Values["response_time"] = 1.0
+
+	require.NoError(t, err1)
+
+	expectedValues := map[string]interface{}{
+		"result_code":   0,
+		"result_type":   "success",
+		"string_found":  true,
+		"response_time": 1.0,
+	}
+	expectedTags := map[string]string{
+		"result_text": "success",
+		"server":      "127.0.0.1",
+		"port":        "2004",
+		"protocol":    "udp",
+	}
+	// Waiting UDPserver
+	wg.Wait()
+	if err := container.Contains(expectedTags, expectedValues); err != nil {
+		t.Errorf("UDP Success failed to set the correct values. Error: %s", err)
+	}
 }
